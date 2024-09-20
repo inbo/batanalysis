5e8a37ec8f1be2fc519cd0597340ab4564177523
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2024-09-20 16:25:44 +0200

    round location coordinates

--- R/read_raw_location.R
+++ R/read_raw_location.R
@@ -6,8 +6,8 @@
 read_raw_location <- function(origin) {
   assert_that(inherits(origin, "Microsoft SQL Server"))
   "SELECT
-  l.id, l.code, l.name, l.parent_id, l.geom.STX AS longitude,
-  l.geom.STY AS latitude
+  l.id, l.code, l.name, l.parent_id, ROUND(l.geom.STX, 5) AS longitude,
+  ROUND(l.geom.STY, 5) AS latitude
 FROM staging_Meetnetten.locations_location AS l
 INNER JOIN staging_Meetnetten.projects_projectlocation AS pl
   ON pl.location_id = l.id
